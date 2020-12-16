156613006d9de7518fd818e4e5b4485bb9e95221
snehadubey9066 <59720911+snehadubey9066@users.noreply.github.com>
2020-12-16 10:36:40 +0530

    deleted unncessary code that can be implemented using callbacks

--- ABS_code.m
+++ ABS_code.m
@@ -1,11 +1,2 @@
-m=75;   % mass of vehicle
-g=9.81; % gravitational constant
-I=5;    % moment of inertia
-R=1.25; % radius of wheel
-v0=44;  % initial vehicle speed
-
-sim("ABS_system.slx");
 open_system("ABS_system");
-open_system("ABS_system/Wheel&VehicleSpeed");
-open_system("ABS_system/VehicleSlip");
-open_system("ABS_system/StoppingDistance");
+sim("ABS_system.slx");
